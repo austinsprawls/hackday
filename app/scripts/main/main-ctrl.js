382374574bfcb47bb37b3ccc37179b07bfefb521
Daniel Cox <draycox@gmail.com>
2014-08-14 16:36:54 -0500

    adding stuff for front page

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -5,7 +5,15 @@ angular.module('rr')
     $scope.selectedCuisine = "";
     $scope.cuisines = ["American", "Chinese", "Greek", "Thai", "Jamacian", "Mexican" ];
 
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
-    });
+    $scope.selectedDistance = "";
+    $scope.distances = ['0-1 Mile', '1-5 Miles', '5+ Miles'];
+
   });
+
+
+
+
+
+// angular.forEach($scope.awesomeThings, function(awesomeThing) {
+//   awesomeThing.rank = Math.random();
+// });
