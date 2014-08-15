a6ce474e51638cf6c1086a200c1bce7672ea3357
Joey G <digitaljedi@outlook.com>
2014-08-14 20:23:12 -0500

    laying basics for input boxes

--- app/scripts/rr.js
+++ app/scripts/rr.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('rr', ['mgcrea.ngStrap','mgcrea.ngStrap.tooltip','mgcrea.ngStrap.typeahead']);
+angular.module('rr', ['mgcrea.ngStrap','mgcrea.ngStrap.tooltip','mgcrea.ngStrap.typeahead', 'debounce']);
 
 
 	// var ipLocate = $.getJSON('http://ip-api.com/json'),
